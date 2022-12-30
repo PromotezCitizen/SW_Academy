c9fd20f9d6b02f23da5c94aba1bead9a9c00663c
PromotezCitizen <gkstkdgus821@gmail.com>
2022-12-30 19:28:23 +0900

    괄호열 계산 끝

--- chap6/pr1.c
+++ chap6/pr1.c
@@ -5,6 +5,18 @@
 char stack[MAX_STACK];
 int idx_stack = 0;
 
+int paran[MAX_STACK] = {0, };
+int idx_paran = 0;
+
+
+void push(int data) {
+    paran[idx_paran++] = data;
+}
+
+int pop() {
+    return paran[--idx_paran];
+}
+
 // 괄호열 계산
 // () : 2
 // [] : 3
@@ -12,43 +24,62 @@ int idx_stack = 0;
 // [X] : X*3
 // ()[] : 2+3
 
+void calcPop(int facter) {
+    int push_data = 0;
+    int data;
+    while ((data = pop()) != 1) {
+        push_data += data;
+    }
+    push_data = push_data == 0 ? 1 : push_data;
+    push(push_data*facter);
+}
+
 int main() {
-    char* str = "(())";
-    char* str1 = "([)]";
+    char* str = "(()[[]])([]))";
 
     int idx = 0;
     int result = 0;
 
-    int sum[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    int idx_sum = 0;
+    int data;
+    int push_data = 0;
+
+    int temp = 0;
+
 
     while (str[idx] != '\0') {
         if (str[idx] == '(') {
-            stack[idx_stack++] = str[idx];
+            stack[idx_stack++] = '(';
+            push(1);
         }
         else if (str[idx] == '[') {
-            stack[idx_stack++] = str[idx];
+            stack[idx_stack++] = '[';
+            push(1);
         }
         else if (str[idx] == ')') {
-            if (stack[--idx_stack] == ']') {
+            if (stack[--idx_stack] == '[') {
                 result = 0;
                 break;
             }
-
-
+            
+            calcPop(2);
         }
         else {
-            if (stack[--idx_stack] == ')') {
+            if (stack[--idx_stack] == '(') {
                 result = 0;
                 break;
             }
 
-
+            calcPop(3);
         }
 
         idx++;
     }
 
+    if (idx_stack == 0) {
+        while(idx_paran != 0) result += pop();
+    }
+    else result = 0;
+    
     printf("%d\n", result);
 
     return 0;
